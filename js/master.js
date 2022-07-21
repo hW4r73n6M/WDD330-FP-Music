4202349933779d06fdb35559c8ca33ff98f19f4e
hW4r73n6M <gui20003@byui.edu>
2022-07-20 22:59:26 -0500

    Search fix

--- js/master.js
+++ js/master.js
@@ -31,26 +31,70 @@ function sectionDisplayControl(elem) {
     }
 };
 
+function clearSection(divId)
+{
+    document.getElementById(divId).innerHTML = "";
+}
+
 addSongToPlaylist = (id) => {
-    console.log('songs: ', songs)
+
     const song = songs.find(s => s.result.id == id)
     const rawPlaylist = localStorage.getItem("hhmusik_playlist.hits") || '[]';
     const playlist = JSON.parse(rawPlaylist);
-    playlist.push(song)
+    let exist = false;
+    let songId = song.result.api_path;
+    console.log("songId: ", songId);
+    for (let i = 0; i < playlist.length; i++) {
+        let playlistSong = playlist[i];
+        console.log("playlistSong.result.api_path: ", playlistSong.result.api_path);
+        if (playlistSong.result.api_path == songId) {
+            exist = true;
+        }
+    }
+    if (!exist) playlist.push(song);
+    localStorage.removeItem('hhmusik_playlist.hits');
     localStorage.setItem('hhmusik_playlist.hits', JSON.stringify(playlist));
 }
 
+/*deleteSongFromPlaylist = (id) => {
+    const song = songs.find(s => s.result.id == id)
+    const rawPlaylist = localStorage.getItem("hhmusik_playlist.hits") || '[]';
+    const playlist = JSON.parse(rawPlaylist);
+    let songId = song.result.api_path;
+    console.log("songId: ", songId);
+    for (let i = 0; i < playlist.length; i++) {
+        let playlistSong = playlist[i];
+        console.log("playlistSong.result.api_path: ", playlistSong.result.api_path);
+        if (playlistSong.result.api_path == songId) {
+            delete playlist[i];
+        }
+    }
+    localStorage.removeItem('hhmusik_playlist.hits');
+    localStorage.setItem('hhmusik_playlist.hits', JSON.stringify(playlist));
+    loadPlaylistData()
+}*/
+
+
 showSongs = (songs, section) => {
     const sectionContainer = section + '-songs';
-    songs.forEach(song => {
-        document.getElementById(sectionContainer).insertAdjacentHTML("afterbegin",
-            `<div class="col-6 space-divs"><div class="row"> <div class="col-6"> <img src="${song.result.song_art_image_thumbnail_url}" class="img-fluid rounded-image"> </div> <div class="col-6"> <h5 class="upper">${song.result.title}</h5> <h6>${song.result.artist_names}</h6> <span>${song.result.release_date_for_display}</span>  <br><br> <span><a href="${song.result.url}" target="_blank">Lyric and 30-second song ↗</a></span> <br><br> <span><button type="button" class="btn btn-outline-info" onclick="addSongToPlaylist('${song.result.id}')">+ Playlist</button></span> </div> </div></div>`);
-    });
+    if (section === 'playlist-section') {
+        songs.forEach(song => {
+            document.getElementById(sectionContainer).insertAdjacentHTML("afterbegin",
+                `<div class="col-6 space-divs"><div class="row"> <div class="col-6"> <img src="${song.result.song_art_image_thumbnail_url}" class="img-fluid rounded-image"> </div> <div class="col-6"> <h5 class="upper">${song.result.title}</h5> <h6>${song.result.artist_names}</h6> <span>${song.result.release_date_for_display}</span>  <br><br> <span><a href="${song.result.url}" target="_blank">Lyric and 30-second song ↗</a></span> <br><br>  </div> </div></div>`);
+        });
+    } else {
+        songs.forEach(song => {
+            document.getElementById(sectionContainer).insertAdjacentHTML("afterbegin",
+                `<div class="col-6 space-divs"><div class="row"> <div class="col-6"> <img src="${song.result.song_art_image_thumbnail_url}" class="img-fluid rounded-image"> </div> <div class="col-6"> <h5 class="upper">${song.result.title}</h5> <h6>${song.result.artist_names}</h6> <span>${song.result.release_date_for_display}</span>  <br><br> <span><a href="${song.result.url}" target="_blank">Lyric and 30-second song ↗</a></span> <br><br> <span><button type="button" class="btn btn-outline-info" onclick="addSongToPlaylist('${song.result.id}')">+ Playlist</button></span> </div> </div></div>`);
+        });
+    }
+
 }
 
 getSongsData = async (searchString, section) => {
     try {
         const newURL = 'https://genius.p.rapidapi.com/search?q=' + searchString;
+        console.log("URL: ", newURL);
         const options = {
             method: 'GET',
             headers: {
@@ -70,15 +114,16 @@ getSongsData = async (searchString, section) => {
 }
 
 function loadHomeData() {
-    const section = 'trendy-section';
-    const searchString = 'Celine Dion';
+    let section = 'trendy-section';
+    let searchString = 'Celine Dion';
     sectionDisplayControl(section);
     getSongsData(searchString, section);
 }
 
 function loadSearchData() {
-    const section = 'search-section';
-    const searchString = document.getElementById("searchString").value;
+    let section = 'search-section';
+    let searchString = document.getElementById("searchString").value;
+    console.log("SEARCH: ", searchString);
     sectionDisplayControl(section);
     getSongsData(searchString, section);
 }
@@ -86,10 +131,16 @@ function loadSearchData() {
 function loadPlaylistData() {
     const section = 'playlist-section';
     sectionDisplayControl(section);
-    const rawPlaylist = localStorage.getItem("hhmusik_playlist.hits");
-    const playlist = JSON.parse(rawPlaylist);
-    showSongs(playlist, section)
+    let rawPlaylist = localStorage.getItem("hhmusik_playlist.hits");
+    let playlist = JSON.parse(rawPlaylist);
+    if(playlist != null) {
+        clearSection('playlist-section-songs');
+        showSongs(playlist, section)
+    }
 }
+
+
+
 
 window.onload = function () {
     loadHomeData();
